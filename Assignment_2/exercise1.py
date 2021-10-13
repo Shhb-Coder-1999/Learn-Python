3a0334b9073cb6b72b79541ce554a5b5ed7b3e55
Shhb <shhb.coder@gmail.com>
2021-10-13 22:55:20 +0330

    finish exercise1

--- Assignment_2/exercise1.py
+++ Assignment_2/exercise1.py
@@ -1,19 +1,49 @@
 import math
 
-print ("please select your operestion /n")
+print ("-----------------------------------------------------------------------")
+print ("please enter your expression : (example 1+2 or sin30) ")
+print ("-----------------------------------------------------------------------")
 
+while True:
+    expression = input ()
+    expression = expression.replace(" ", "")
+    
+    if expression.find('+') == True:
+        result = expression.split('+')
+        print (float(result [0]),"+", float(result[1]) , "=" , float(result[1]) + float(result [0]))
+        print ("------------------------------")
 
-print ("+ => sum")
-print ("- => subtraction")
-print ("/ => divide")
-print ("* => multiplication")
-print ("sin => sin")
-print ("cos => cos")
-print ("cot => cot")
-print ("tan => tan")
-print ("log => log")
+    elif expression.find('-')==True:   
+        result = expression.split('-')
+        print (float(result [0]),"-", float(result[1]) , "=" , float(result[0]) - float(result [1]))
+        print ("------------------------------")
 
+    elif expression.find('/')==True:   
+        result = expression.split('/')
+        print (float(result [0]),"/", float(result[1]) , "=" , float(result[0]) / float(result [1]))
+        print ("------------------------------")
 
+    elif expression.find("sin") == 0:  
+        result = expression.split("sin")
+        print (math.sin(math.radians(float(result[1]))))
+        print ("------------------------------")  
 
-height = input ()
-print (float(height.split('+')[0].strip().replace(" ", "")) ,"+", float(height.split("+",1)[1].replace(" ", "")) , "=" , 4 )
+    elif expression.find("cos") == 0:  
+        result = expression.split("cos")
+        print (math.cos(math.radians(float(result[1]))))
+        print ("------------------------------")  
+
+    elif expression.find("tan") == 0:  
+        result = expression.split("tan")
+        print (math.tan(math.radians(float(result[1]))))
+        print ("------------------------------")  
+
+    elif expression.find("cot") == 0:  
+        result = expression.split("cot")
+        print (math.sin(1/math.radians(float(result[1]))))
+        print ("------------------------------")  
+
+    elif expression.find("log") == 0:  
+        result = expression.split("log")
+        print (math.log(float(result[1])))
+        print ("------------------------------")                
