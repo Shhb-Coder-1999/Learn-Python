0605edaf1e8aa84371ce2bd1d98c23432f305130
Shhb <shhb.coder@gmail.com>
2021-10-19 14:19:35 +0330

    create and finish exercise7

---
+++ Assignment_3/exercise7.py
@@ -0,0 +1,6 @@
+import math 
+num1= int(input ("enter your first number :")) 
+num2= int(input ("enter your second number :")) 
+print(" GCD of two number ", num1 ," and ", num2 , " is ", math.gcd(num1, num2))  
+
+
