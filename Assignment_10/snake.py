0dd9f6f1dbf412a8681cf568f4429a0d2263f960
Shhb <shhb.coder@gmail.com>
2021-12-06 22:43:34 +0330

    add pear & hitch & score

--- Assignment_10/snake.py
+++ Assignment_10/snake.py
@@ -1,11 +1,16 @@
 import random
 import arcade
+import arcade.gui
 from arcade.key import RIGHT
 from arcade.window_commands import start_render
 from pyglet.libs.x11.xlib import Screen
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 1000
+
+class QuitButton(arcade.gui.UIFlatButton):
+    def on_click(self, event: arcade.gui.UIOnClickEvent):
+        arcade.exit()
 
 
 class Snake(arcade.Sprite):
@@ -23,8 +28,17 @@ class Snake(arcade.Sprite):
     def move (self):
        pass
 
-    def eat (self):
-       pass  
+    def eat (self , obj_x , obj_y , obj_type):
+       if self.center_y == obj_y and self.center_x  == obj_x :
+         if obj_type == 'apple':      
+           self.score = self.score + 1
+           return True
+         elif obj_type == 'pear':
+           self.score = self.score + 2
+           return True 
+         elif obj_type == 'hitch':
+           self.score = self.score - 1  
+           return True       
 
     def draw (self):
        arcade.draw_rectangle_filled(self.center_x , self.center_y , self.width , self.height , self.color)  
@@ -34,17 +48,36 @@ class Snake(arcade.Sprite):
 class Apple(arcade.Sprite):
      def __init__(self):
         super().__init__()
-        self.width = 10
-        self.height = 10
-        self.color = arcade.color.RED
-        self.change_x = None
-        self.change_y = None
-        self.score = 0
-        self.center_x = SCREEN_WIDTH //2
-        self.center_y = SCREEN_HEIGHT //2
+        self.image = '/media/shb/3C50BD1450BCD63C/programming/Github/Learn_Python/Assignment_10/apple.png'
+        self.apple = arcade.Sprite(self.image, 0.10)
+        self.apple.center_x  = random.randrange(60, SCREEN_HEIGHT , 20) 
+        self.apple.center_y  = random.randrange(60, SCREEN_WIDTH , 20) 
 
-     def draw (self):
-           arcade.draw_circle_filled(self.center_x +40, self.center_y+40 ,radius=10 , color=self.color)     
+     def draw(self):
+        self.apple.draw()
+
+class Pear(arcade.Sprite):
+     def __init__(self):
+        super().__init__()
+        self.image = '/media/shb/3C50BD1450BCD63C/programming/Github/Learn_Python/Assignment_10/pear.png'
+        self.pear = arcade.Sprite(self.image, 0.08)
+        self.pear.center_x  = random.randrange(60, SCREEN_HEIGHT , 20) 
+        self.pear.center_y  = random.randrange(60, SCREEN_WIDTH , 20) 
+
+     def draw(self):
+        self.pear.draw()  
+
+class Hitch(arcade.Sprite):
+     def __init__(self):
+        super().__init__()
+        self.image = '/media/shb/3C50BD1450BCD63C/programming/Github/Learn_Python/Assignment_10/shit.png'
+        self.hitch = arcade.Sprite(self.image, 0.08)
+        self.hitch.center_x  = random.randrange(60, SCREEN_HEIGHT , 20) 
+        self.hitch.center_y  = random.randrange(60, SCREEN_WIDTH , 20) 
+
+     def draw(self):
+        self.hitch.draw()                
+             
 
 class Game(arcade.Window):
      def __init__(self):
@@ -52,23 +85,53 @@ class Game(arcade.Window):
        arcade.set_background_color(arcade.color.SAND)
        self.snake = Snake()
        self.apple = Apple()
+       self.pear = Pear()
+       self.hitch = Hitch()
+       
 
      def on_draw(self):
         arcade,start_render()
         arcade.draw_text('Score : '+str(self.snake.score),start_x=20 , start_y=40 , font_size=30) 
         self.snake.draw()
         self.apple.draw()
-
+        self.pear.draw()
+        self.hitch.draw()
+     
      def on_key_release(self, symbol: int, modifiers: int):
          if  symbol == arcade.key.LEFT:
-            print(symbol)
             self.snake.center_x -= 20
+            self.check_pos()
+                
          elif symbol == arcade.key.RIGHT:
             self.snake.center_x += 20
+            self.check_pos()
+
          elif symbol == arcade.key.UP:
             self.snake.center_y += 20
-         elif  symbol == arcade.key.DOWN: 
+            self.check_pos()
+
+         elif symbol == arcade.key.DOWN:
             self.snake.center_y -= 20
+            self.check_pos()
+             
+     def check_pos(self):
+            
+        if self.snake.eat(self.apple.apple.center_x , self.apple.apple.center_y ,'apple') == True:  
+                  self.apple.apple.center_x = random.randrange(60, SCREEN_HEIGHT - self.apple.center_x , 20) 
+                  self.apple.apple.center_y = random.randrange(60, SCREEN_WIDTH - self.apple.center_y , 20)
+
+        elif self.snake.eat(self.pear.pear.center_x , self.pear.pear.center_y ,'pear') == True:  
+                  self.pear.pear.center_x = random.randrange(60, SCREEN_HEIGHT - self.pear.center_x , 20) 
+                  self.pear.pear.center_y = random.randrange(60, SCREEN_WIDTH - self.pear.center_y , 20)
+
+        elif self.snake.eat(self.hitch.hitch.center_x , self.hitch.hitch.center_y ,'hitch') == True:  
+                  self.hitch.hitch.center_x = random.randrange(60, SCREEN_HEIGHT - self.hitch.center_x , 20) 
+                  self.hitch.hitch.center_y = random.randrange(60, SCREEN_WIDTH - self.hitch.center_y , 20)                    
+
+
+
+       
+
 
 
 my_game = Game()
