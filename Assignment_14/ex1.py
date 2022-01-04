4cc60dbdc43ee576bd7e3c72179df2964f3538f4
shahab shafie <80480584+Shhb-Coder-1999@users.noreply.github.com>
2022-01-04 19:23:04 +0330

    create and finish ex1

---
+++ Assignment_14/ex1.py
@@ -0,0 +1,267 @@
+from math import *
+from PySide6.QtWidgets import *
+from PySide6.QtUiTools import *
+from PySide6.QtCore import *
+from PySide6.QtUiTools import  QUiLoader
+from functools import partial
+
+class Calculator(QMainWindow):
+  def __init__(self):
+      super().__init__()
+      self.input_num = None
+      self.result = 0
+      self.last_op = None
+      loader = QUiLoader()
+      self.ui = loader.load("cal_design.ui")
+      self.ui.show()
+
+      self.ui.num_1.clicked.connect(partial(self.init, '1' ,'num'))
+      self.ui.num_2.clicked.connect(partial(self.init, '2' ,'num'))
+      self.ui.num_3.clicked.connect(partial(self.init, '3' ,'num'))
+      self.ui.num_4.clicked.connect(partial(self.init, '4' ,'num'))
+      self.ui.num_5.clicked.connect(partial(self.init, '5' ,'num'))
+      self.ui.num_6.clicked.connect(partial(self.init, '6' ,'num'))
+      self.ui.num_7.clicked.connect(partial(self.init, '7' ,'num'))
+      self.ui.num_8.clicked.connect(partial(self.init, '8' ,'num'))
+      self.ui.num_9.clicked.connect(partial(self.init, '9' ,'num'))
+      self.ui.num_0.clicked.connect(partial(self.init, '0' ,'num'))
+      self.ui.dot_btn.clicked.connect(partial(self.init, '.' ,'num'))
+      self.ui.sum_btn.clicked.connect(partial(self.init, None ,'sum'))
+      self.ui.sub_btn.clicked.connect(partial(self.init, None ,'sub'))
+      self.ui.div_btn.clicked.connect(partial(self.init, None ,'div'))
+      self.ui.mul_btn.clicked.connect(partial(self.init, None ,'mul'))
+      self.ui.res_btn.clicked.connect(partial(self.init, None ,'res'))
+      self.ui.sin_btn.clicked.connect(partial(self.init, None ,'sin'))
+      self.ui.cos_btn.clicked.connect(partial(self.init, None ,'cos'))
+      self.ui.tan_btn.clicked.connect(partial(self.init, None ,'tan'))
+      self.ui.cot_btn.clicked.connect(partial(self.init, None ,'cot'))
+      self.ui.sqrt_btn.clicked.connect(partial(self.init, None ,'sqrt'))
+      self.ui.log_btn.clicked.connect(partial(self.init, None ,'log'))
+      self.ui.symmetry_btn.clicked.connect(partial(self.init, None ,'symmetry'))
+      self.ui.clear_btn.clicked.connect(partial(self.init, None ,'clear'))
+      
+
+  def init(self , num , type):
+      if(type == 'num'):
+        if self.input_num == None :
+            self.input_num = num
+            self.ui.lcdNumber.display(self.input_num)
+        else:
+            if num == '.' and "." not in self.input_num:
+                self.input_num += num
+                self.ui.lcdNumber.display(self.input_num)  
+            elif num != '.':
+                self.input_num += num
+                self.ui.lcdNumber.display(self.input_num) 
+
+
+      elif  type == 'sum':
+         if self.last_op != 'equal' and self.last_op != 'sin' and self.last_op != 'cos' and self.last_op != 'tan' and self.last_op != 'cot' and self.last_op != 'log' and self.last_op != 'sqrt' and self.last_op != 'symmetry'  :
+           self.sum()
+         else :
+                self.ui.lcdNumber.display("0") 
+                self.input_num = None
+                self.last_op = 'sum'     
+
+      elif  type == 'sub' :
+         if self.last_op != 'equal' and self.last_op != 'sin' and self.last_op != 'cos' and self.last_op != 'tan' and self.last_op != 'cot' and self.last_op != 'log' and self.last_op != 'sqrt' and self.last_op != 'symmetry' :
+            self.sub()
+         else :
+                self.ui.lcdNumber.display("0") 
+                self.input_num = None
+                self.last_op = 'sub'   
+
+      elif  type == 'mul' :
+          if self.last_op != 'equal' and self.last_op != 'sin' and self.last_op != 'cos' and self.last_op != 'tan' and self.last_op != 'cot' and self.last_op != 'log' and self.last_op != 'sqrt' and self.last_op != 'symmetry' :
+             self.mul()
+          else :
+                self.ui.lcdNumber.display("0") 
+                self.input_num = None
+                self.last_op = 'mul' 
+
+      elif  type == 'div' :
+          if self.last_op != 'equal' and self.last_op != 'sin' and self.last_op != 'cos' and self.last_op != 'tan' and self.last_op != 'cot' and self.last_op != 'log' and self.last_op != 'sqrt' and self.last_op != 'symmetry' :
+             self.div()
+          else :
+                self.ui.lcdNumber.display("0") 
+                self.input_num = None
+                self.last_op = 'div' 
+
+      elif  type == 'sin':
+          if self.last_op != 'equal':
+             self.sin()
+          else :
+                self.ui.lcdNumber.display("0") 
+                self.input_num = None
+                self.last_op = 'sin' 
+
+      elif  type == 'cos':
+          if self.last_op != 'equal':
+             self.cos()
+          else :
+                self.ui.lcdNumber.display("0") 
+                self.input_num = None
+                self.last_op = 'cos'  
+
+      elif  type == 'tan':
+          if self.last_op != 'equal':
+             self.tan()
+          else :
+                self.ui.lcdNumber.display("0") 
+                self.input_num = None
+                self.last_op = 'tan'  
+
+      elif  type == 'cot':
+          if self.last_op != 'equal':
+             self.cot()
+          else :
+                self.ui.lcdNumber.display("0") 
+                self.input_num = None
+                self.last_op = 'cot'  
+
+      elif  type == 'sqrt':
+          if self.last_op != 'equal':
+             self.sqrt()
+          else :
+                self.ui.lcdNumber.display("0") 
+                self.input_num = None
+                self.last_op = 'sqrt'  
+
+      elif  type == 'log':
+          if self.last_op != 'equal':
+             self.log()
+          else :
+                self.ui.lcdNumber.display("0") 
+                self.input_num = None
+                self.last_op = 'log'        
+
+      elif  type == 'symmetry':
+                self.symmetry()
+                self.last_op = 'symmetry'  
+
+      elif  type == 'clear':
+                self.clear()
+                self.last_op = 'clear'                                     
+                
+
+      elif  type == 'res':
+          self.equal()    
+          self.ui.lcdNumber.display(self.result) 
+
+
+  def sum(self):
+       if self.result !=0:
+        self.result = self.result + float(self.input_num)
+       else :
+        self.result = float(self.input_num)
+       
+       self.ui.lcdNumber.display("0") 
+       self.input_num = None
+       self.last_op = 'sum'
+
+  def sub(self):
+       if self.result !=0:
+        self.result = self.result - float(self.input_num)
+       else :
+        self.result = float(self.input_num)
+       
+       self.ui.lcdNumber.display("0") 
+       self.input_num = None
+       self.last_op = 'sub'
+
+  def mul(self):
+       if self.result !=0 :
+        self.result = self.result * float(self.input_num)
+       else :
+          self.result = float(self.input_num)
+       self.ui.lcdNumber.display("0") 
+       self.input_num = None
+       self.last_op = 'mul'
+
+  def div(self):
+       if self.result !=0:
+        self.result = self.result / float(self.input_num)
+       else :
+        self.result = float(self.input_num)
+       
+       self.ui.lcdNumber.display("0") 
+       self.input_num = None
+       self.last_op = 'div'
+
+  def sin(self):
+       self.result = sin(radians(float(self.input_num)))
+       self.ui.lcdNumber.display(self.result) 
+       self.input_num = None
+       self.last_op = 'sin'
+
+  def cos(self):
+       self.result = cos(radians(float(self.input_num)))
+       self.ui.lcdNumber.display(self.result) 
+       self.input_num = None
+       self.last_op = 'cos'   
+
+  def tan(self):
+       self.result = tan(radians(float(self.input_num)))
+       self.ui.lcdNumber.display(self.result) 
+       self.input_num = None
+       self.last_op = 'tan'
+
+  def cot(self):
+       self.result = 1/tan(radians(float(self.input_num)))
+       self.ui.lcdNumber.display(self.result) 
+       self.input_num = None
+       self.last_op = 'cot' 
+
+  def log(self):
+       self.result = log(float(self.input_num))
+       self.ui.lcdNumber.display(self.result) 
+       self.input_num = None
+       self.last_op = 'log'  
+
+  def sqrt(self):
+       self.result = sqrt(float(self.input_num))
+       self.ui.lcdNumber.display(self.result) 
+       self.input_num = None
+       self.last_op = 'sqrt' 
+
+  def symmetry(self):
+       if self.input_num[0]  == '-':
+            self.result = float(self.input_num[1:])
+            self.ui.lcdNumber.display(self.result) 
+            self.input_num = None
+            self.last_op = 'symmetry' 
+       else: 
+           self.result =float("-" + self.input_num)
+           self.ui.lcdNumber.display(self.result) 
+           self.input_num = None
+           self.last_op = 'symmetry' 
+
+  def clear(self):
+       self.input_num = None
+       self.result = 0
+       self.ui.lcdNumber.display(self.result) 
+       self.last_op = 'symmetry' 
+
+
+
+  def equal(self):
+      if self.last_op == 'sum':
+       self.result = self.result + float(self.input_num)
+       self.last_op = 'equal'
+      elif self.last_op == 'sub':
+       self.result = self.result - (float(self.input_num))
+       self.last_op = 'equal'
+      elif self.last_op == 'mul':
+       self.result = self.result * float(self.input_num) 
+       self.last_op = 'equal'
+      elif self.last_op == 'div':
+       self.result = self.result / float(self.input_num) 
+       self.last_op = 'equal'     
+         
+
+
+
+my_app = QApplication()
+calculator = Calculator()
+calculator.setFixedSize(200 , 300)
+my_app.exec()
